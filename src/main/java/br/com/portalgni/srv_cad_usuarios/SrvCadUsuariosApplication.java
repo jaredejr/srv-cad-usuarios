7512279c101ea509cab726fe15fe9cee5fe80a90
jaredejr <jaredejr@gmail.com>
2024-07-25 22:14:45 -0300

    comit inicial do projeto

---
+++ src/main/java/br/com/portalgni/srv_cad_usuarios/SrvCadUsuariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.portalgni.srv_cad_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvCadUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvCadUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portalgni/srv_cad_usuarios/SrvCadUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portalgni.srv_cad_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvCadUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
