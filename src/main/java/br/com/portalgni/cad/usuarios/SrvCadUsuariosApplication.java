2fb27e5a3124cfb65ca7d24c46a5d76be135f113
jaredejr <jaredejr@gmail.com>
2025-06-06 23:26:20 -0300

    adicionando release no log

--- src/main/java/br/com/portalgni/cad/usuarios/SrvCadUsuariosApplication.java
+++ src/main/java/br/com/portalgni/cad/usuarios/SrvCadUsuariosApplication.java
@@ -1,13 +1,28 @@
 package br.com.portalgni.cad.usuarios;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SrvCadUsuariosApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SrvCadUsuariosApplication.class);
+
+	@Value("${app.name}")
+	private static String appName;
+
+	@Value("${app.version}")
+	private static String appVersion;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SrvCadUsuariosApplication.class, args);
+
+		logger.info("Aplicação: {} - Release: {}", appName, appVersion);
+
 	}
 
 }
